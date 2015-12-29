44a0f09d653049ed823901e2a8fc5b4f0d2bb675
APEFind <apefind@gmail.com>
2015-12-29 23:15:56 +0100

    ...

---
+++ shellutil/doc.go
@@ -0,0 +1,14 @@
+/*
+Package shellutil contains a few useful shell utilities:
+
+	prompt:
+		A nice shell prompt
+	timout:
+		Run a system command under a timeout
+	timeit:
+		Run a cmd multiple times
+	cleanup:
+		Remove certain files from directory
+*/
+
+package shellutil
